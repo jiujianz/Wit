57bc402f35cf0ef174b51b760cb3f265ac1e9f79
jiujianz <cocobskg@gmail.com>
2020-07-12 23:40:39 +0900

    arrange file structure

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import(
+	"./controller"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", controller.IndexGET)
+	router.Run(":3000")
+}
