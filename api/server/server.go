0c5e6a1c723bda90dcb0008a668c9c063354c8a9
jiujianz <cocobskg@gmail.com>
2020-10-14 23:13:30 +0900

    fix server.go

--- api/server/server.go
+++ api/server/server.go
@@ -1,15 +1,20 @@
 package server
 
 import (
-	"../controller"
+	"net/http"
 
+	"../controller"
 	"github.com/gin-gonic/gin"
 )
 
 func Router() {
 	router := gin.Default()
-
 	v1 := router.Group("/api/v1")
+
+	router.LoadHTMLFiles("UI/index.html")
+	v1.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", map[string]interface{}{})
+	})
 	{
 		v1.POST("/signup", controller.SignUp())
 		//server.GET("/signin", controller.SignUp())
