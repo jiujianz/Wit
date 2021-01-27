4ace5a3e48f394744bfc219e2c5b6a5fd14d067d
jiujianz <cocobskg@gmail.com>
2021-01-27 22:07:56 +0900

    add comment to userModel.go

--- api/Models/UserModel.go
+++ api/Models/UserModel.go
@@ -2,30 +2,34 @@ package models
 
 import (
 	DBConfig "../Config"
+	Utility "../Utility"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 )
 
 type User struct {
-	DBConfig.Model
-	LoginID  string `json："LoginId"`
+	LoginID  string `json："loginId"`
 	Password string `json："password"`
 	Email    string `json："email"`
 	UserName string `json："userName"`
 	ToakenID string `json："toakenId"`
 }
 
+// TableName テーブル名をreturn
 func (b *User) TableName() string {
 	return "user"
 }
 
+// CreateUser ユーザー追加
 func CreateUser(user *User) (err error) {
+	Utility.PasswordEncryption(user.Password)
 	if err = DBConfig.DB.Create(user).Error; err != nil {
 		return err
 	}
 	return nil
 }
 
+// GetUserByLoginID ログインIDからユーザーを参照
 func GetUserByLoginID(user *User) (err error) {
 	loginID := user.LoginID
 	password := user.Password
